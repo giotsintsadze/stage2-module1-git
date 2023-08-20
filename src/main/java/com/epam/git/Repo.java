069609d7d37e75e2427b80a28b88aec88aafc1db
Giorgi Tsintsadze <giotsintsadze94@gmail.com>
2023-08-20 21:59:18 +0400

    redone task

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/giotsintsadze/taskgit";
+    public static String REPO_LINK = "https://github.com/giotsintsadze/gitTask2";
 }
